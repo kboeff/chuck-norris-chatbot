7575176f14230422d21266549eeffaf6c3428626
unknown <kamen.boev@gmail.com>
2019-03-06 03:40:08 +0200

    fixes

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function handleMessage(sender_psid, time_stamp, received_message) {
     request('http://api.icndb.com/jokes/random/', function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
+        // console.log('body:', body);
         joke = body["value"]["joke"];
     });
     
