d5d28f13cee1600e28beeea5bbab06efd19bbdb6
Kamen Boev <kamen.boev@gmail.com>
2019-03-11 11:08:33 +0000

    tset

--- index.js
+++ index.js
@@ -152,7 +152,9 @@ function callSendAPI(sender_psid, response) {
     "recipient": {
       "id": sender_psid
     },
-    "message": response
+    "message": {
+        "text": response
+    }
   };
 
   // Send the HTTP request to the Messenger Platform
